dcdd3f3a4d4ed148ce32932f6e07560f50256b39
Urooj Fatima <uroojfsadiq@gmail.com>
2020-03-26 00:30:07 +0500

    sidebar working

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <React.Fragment>
+      <h1>Home Page - Front End</h1>
+      <a href="/admin">Admin Panel</a>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import adminPanel from "./components/adminPanel";
+import AdminBar from "./components/adminComponents/adminBar";
+import AdminDashboard from "./components/adminComponents/adminDashboard";
+import AdminManage from "./components/adminComponents/adminManage";
+import ManageNavbar from "./components/adminComponents/manageNavbar";
+import HomeSec01 from "./components/adminComponents/homeSec01";
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
-      <Route path="/admin" component={adminPanel} />
+      <Route exact path="/admin" component={AdminBar} />
+      <Route exact path="/admin/dashboard" component={AdminDashboard} />
+      <Route exact path="/admin/manage" component={AdminManage} />
+      <Route exact path="/admin/navbar" component={ManageNavbar} />
+      <Route exact path="/admin/homepage-section01" component={HomeSec01} />
     </div>
   </Router>
 );
